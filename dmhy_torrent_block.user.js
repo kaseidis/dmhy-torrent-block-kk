09e7987f443bd18458256c0fc85a52e27940c7a2
kaseidis <k@kcl.moe>
2026-07-29 20:07:01 -0400

    perf: preload CMS content pages

--- dmhy_torrent_block.user.js
+++ dmhy_torrent_block.user.js
@@ -1815,7 +1815,8 @@ class LinkPreloader {
         if (url.origin !== location.origin || !/^https?:$/.test(url.protocol)) return false;
         const isTopicDetail = /\/topics\/view\//.test(url.pathname);
         const isPagination = /^\/topics\/list(?:\/[^/]+)*\/page\/\d+\/?$/.test(url.pathname);
-        if (!isTopicDetail && !isPagination) return false;
+        const isCmsPage = /^\/cms\/page\/.+/.test(url.pathname);
+        if (!isTopicDetail && !isPagination && !isCmsPage) return false;
         if (url.pathname === location.pathname && url.search === location.search) return false;
         return true;
     }
